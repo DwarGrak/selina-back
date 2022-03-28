143187793b6fbdbec5e31c6fb87070c369089511
Nikita Biryukov <dwarfgrakula@gmail.com>
2022-03-28 18:34:50 +0300

    Improve error message in ParseDatePipe

--- src/pipes/parse_date.pipe.ts
+++ src/pipes/parse_date.pipe.ts
@@ -1,12 +1,20 @@
-import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
 
 @Injectable()
 export class ParseDatePipe implements PipeTransform {
-  transform(value: any) {
+  transform(value: any, metadata: ArgumentMetadata) {
     const check = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/.test(
       value,
     );
-    if (!check) throw new BadRequestException('Is not a date');
+    if (!check)
+      throw new BadRequestException(
+        `"${metadata.data}" should be a date`,
+      );
     return value;
   }
 }
